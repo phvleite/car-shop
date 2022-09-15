97d19e66a55105fa35a4fd929725b8680b5a0a9d
phvleite <phvleite@gmail.com>
2022-09-15 09:39:10 -0300

    Realizado o service Car

---
+++ src/services/Car.ts
@@ -0,0 +1,60 @@
+import { z } from 'zod';
+import { IService } from '../interfaces/IService';
+import { ICar, CarZodSchema } from '../interfaces/ICar';
+import { VehicleZodSchema } from '../interfaces/IVehicle';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+
+class CarService implements IService<ICar> {
+  constructor(private _car: IModel<ICar>) {}
+
+  public async create(obj: unknown): Promise<ICar> {
+    const parsed = z.intersection(CarZodSchema, VehicleZodSchema).safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+
+    const created = await this._car.create(parsed.data);
+
+    return created;
+  }
+
+  public async read(): Promise<ICar[]> {
+    const result = await this._car.read();
+
+    return result;
+  }
+
+  public async readOne(_id: string): Promise<ICar | null> {
+    const car = await this._car.readOne(_id);
+
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+
+    return car;  
+  }
+
+  public async update(_id: string, obj: unknown): Promise<ICar | null> {
+    const parsed = z.intersection(CarZodSchema, VehicleZodSchema).safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+
+    const updated = await this._car.update(_id, parsed.data);
+
+    if (!updated) throw new Error(ErrorTypes.EntityNotFound);
+
+    return updated;
+  }
+
+  public async delete(_id: string): Promise<ICar | null> {
+    const deleted = await this._car.delete(_id);
+    
+    if (!deleted) throw new Error(ErrorTypes.EntityNotFound);
+
+    return deleted;
+  }
+}
+
+export default CarService;
