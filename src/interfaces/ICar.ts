cb8b0cda0d28d7caa7631285aac2ef5e153a294d
phvleite <phvleite@gmail.com>
2022-09-14 21:32:00 -0300

    req 03 realizado

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = z.object({
+  doorsQty: z.number({
+    required_error: 'Doors quantity is required',
+    invalid_type_error: 'Doors quantity must be a number',
+  }).int({
+    message: 'Doors quantity must be integer',
+  }).gte(2, {
+    message: 'Doors quantity must be greater than or equal to 2',
+  }).lte(4, {
+    message: 'Doors quantity must be less than or equal to 4',
+  }),
+
+  seatsQty: z.number({
+    required_error: 'Seats quantity is required',
+    invalid_type_error: 'Seats quantity must be a number',
+  }).int({
+    message: 'Seats quantity must be integer',
+  }).gte(2, {
+    message: 'Seats quantity must be greater than or equal to 2',
+  }).lte(7, {
+    message: 'Seats quantity must be less than or equal to 7',
+  }),
+});
+
+export type ICar = z.infer<typeof CarZodSchema & typeof VehicleZodSchema>;
+export { CarZodSchema };
